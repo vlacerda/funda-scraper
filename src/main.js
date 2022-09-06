4c21e00c2a237831664be39ad1f4976dc2bc3364
Vinicius de Lacerda <vinicius@findhotel.net>
2022-09-06 21:12:54 +0200

    Updated URLS

--- src/main.js
+++ src/main.js
@@ -16,8 +16,7 @@ const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
 const urls = [
-    'https://www.funda.nl/en/koop/amsterdam/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
-    'https://www.funda.nl/en/koop/haarlem/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
+    'https://www.funda.nl/huur/amsterdam/beschikbaar/0-1750/5-dagen/'
 ];
 
 const runTask = async () => {
